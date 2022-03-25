527da08906718be5c187754c6c3f6c3b82c117be
SreejaPaulG41 <sreejap@geekyants.com>
2022-03-25 17:54:32 +0530

    URL Typo Fixed

--- src/Saga/Response/genreBasedQuestionResponse.tsx
+++ src/Saga/Response/genreBasedQuestionResponse.tsx
@@ -5,7 +5,7 @@ const genreBasedQuestionResponse = (payload: string) =>{
     const dataToSend = {"genreId": payload}
     return axios.request({
         method: 'GET',
-        url: `${process.env.REACT_APP_API_URL}genreBasedQuestions1`,
+        url: `${process.env.REACT_APP_API_URL}genreBasedQuestions`,
         headers: {
             'Content-Type': 'application/json',
             'jwtToken': localStorage.token
